bf287eb066e2df0cb682d0c4e597851e2a74be48
Hans Song <hans-m-song@gmail.com>
2018-12-25 19:52:42 +1000

    website build

---
+++ precache-manifest.1de2edba61db3b187239bca55e169491.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e58bc246c14e2aa8b9d7fe7b5bbff30e",
+    "url": "/static/media/mugshot.e58bc246.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9cc72f68760560b60cbc",
+    "url": "/static/js/main.9cc72f68.chunk.js"
+  },
+  {
+    "revision": "a4ebaa50dadde172008d",
+    "url": "/static/js/1.a4ebaa50.chunk.js"
+  },
+  {
+    "revision": "9cc72f68760560b60cbc",
+    "url": "/static/css/main.22fed102.chunk.css"
+  },
+  {
+    "revision": "b973e134cb5f2c5c414cc8445f72f39c",
+    "url": "/index.html"
+  }
+];
