4f6ab1e323c5a50f8f3caade4a4da89e2e06404a
hans song <hans.m.song@gmail.com>
2019-01-18 15:51:51 +1000

    Updates

---
+++ precache-manifest.eb40a1953b23d97035a4abe4e44e00e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e58bc246c14e2aa8b9d7fe7b5bbff30e",
+    "url": "/static/media/mugshot.e58bc246.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d8374d0ae643397bd2ce",
+    "url": "/static/js/main.d8374d0a.chunk.js"
+  },
+  {
+    "revision": "a4ebaa50dadde172008d",
+    "url": "/static/js/1.a4ebaa50.chunk.js"
+  },
+  {
+    "revision": "d8374d0ae643397bd2ce",
+    "url": "/static/css/main.8d08a87f.chunk.css"
+  },
+  {
+    "revision": "1660c1e2042304dd4a6d7274ac2d5612",
+    "url": "/index.html"
+  }
+];
