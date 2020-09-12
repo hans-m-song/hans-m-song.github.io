bfe676231d26e2c2b4d5172c3e4955f57fa3a0ec
Hans Song <hans.m.song@gmail.com>
2020-09-12 15:43:23 +1000

    update dependencies and info

--- src/Info.js
+++ src/Info.js
@@ -1,8 +1,9 @@
 export const profile = {
-  name: "Hans Song",
-  bio: "Final year studying Software Engineering at the University of Queensland",
-  image: "",
-  resume: "",
+  name: 'Hans Song',
+  bio:
+    'Final year studying Software Engineering at the University of Queensland',
+  image: '',
+  resume: '',
 };
 
 export const education = [
@@ -14,14 +15,14 @@ export const education = [
   },
   */
   {
-    location: "University of Queensland",
-    certification: "Bachelor of Engineering (Software)",
-    period: ["2016 Feb", "Present"],
+    location: 'University of Queensland',
+    certification: 'Bachelor of Engineering (Software)',
+    period: ['2016 Feb', 'Present'],
   },
   {
-    location: "Queensland Academy for Science, Math, and Technology",
-    certification: "International Baccalaureate Diploma",
-    period: ["2013 Jan", "2015 Nov"],
+    location: 'Queensland Academy for Science, Math, and Technology',
+    certification: 'International Baccalaureate Diploma',
+    period: ['2013 Jan', '2015 Nov'],
   },
 ];
 
@@ -32,31 +33,54 @@ export const skills = [
   //     list: [],
   // },
   {
-    title: "Software Development",
-    description: "Experience in OOP, imperative, and functional programming paradigm. Experience in standard frontend and backend website development. Minor experience with embeded development.",
-    list: ["Python", "Java", "C", "JavaScript/TypeScript", "HTML/CSS", "SQL", "MatLab"],
+    title: 'Frontend Development',
+    description:
+      'Experience in standard frontend and backend website development',
+    list: ['HTML/CSS', 'JavaScript/Typescript', 'React.Js', 'Vue.Js'],
   },
   {
-    title: "Software Libraries",
-    description: "Familiar with popular libraries and frameworks.",
-    list: ["JUnit", "Express.js", "Node.js", "React.js", "Puppeteer", "RabbitMQ", "MongoDB", "Jest/Mocha+Chai"],
+    title: 'Backend Development',
+    description:
+      'Experience in hosting and serving web services and common deployment tools',
+    list: [
+      'Docker',
+      'Docker Compose',
+      'Nginx',
+      'JavaScript/TypeScript',
+      'Express.js',
+      'Node.js',
+    ],
   },
   {
-    title: "3D Modelling and Printing",
-    description: "Familiar with CAD and constrained modelling of dimensionally accurate models for printing on hand-built 3D printer.",
-    list: ["AutoCAD", "Inventor", "Fusion360"],
+    title: 'Software Development',
+    description:
+      'Experience in OOP, imperative, and functional programming paradigm. Minor experience with embeded development.',
+    list: ['Python', 'Java', 'C', 'HTML/CSS', 'SQL', 'MatLab'],
   },
   {
-    title: "Languages",
-    description: "Proficient in Mandarin Chinese.",
-    list: ["HSK 4"],
+    title: 'Software Libraries',
+    description: 'Familiar with popular libraries and frameworks.',
+    list: [
+      'JUnit',
+      'Express.js',
+      'Puppeteer',
+      'Playwright',
+      'RabbitMQ',
+      'MongoDB',
+      'Jest/Mocha+Chai',
+    ],
   },
   {
-    title: "Machine Learning",
-    description: "Some background in supervised and unsupervised training algorithms, image processing, and CNNs.",
-    list: ["TensorFlow", "MatLab"],
-  }
-
+    title: '3D Modelling and Printing',
+    description:
+      'Familiar with CAD and constrained modelling of dimensionally accurate models for printing on hand-built 3D printer.',
+    list: ['AutoCAD', 'Inventor', 'Fusion360'],
+  },
+  {
+    title: 'Languages',
+    description: 'Proficient in Mandarin Chinese.',
+    list: ['HSK 4'],
+  },
 ];
 
 export const experience = [
@@ -70,53 +94,75 @@ export const experience = [
   },
   */
   {
-    location: "Jacaranda Finance",
-    title: "Junior Data Mining Analyst",
-    period: ["2019 Jan", "Present"],
-    skills: ["Node.js", "JavaScript", "TypeScript", "Puppeteer", "SQL"],
-    description: "Creating and maintaining autonomous data aggregation modules utilizing puppeteer.",
+    location: 'Credfin Australia',
+    title: 'Software Developer',
+    period: ['2019 Jan', 'Present'],
+    skills: [
+      'Node.js',
+      'JavaScript',
+      'TypeScript',
+      'Puppeteer',
+      'Playwright',
+      'Vue.js',
+      'SQL',
+    ],
+    description:
+      'Creating and maintaining autonomous data aggregation modules utilizing puppeteer and creating tools for developers.',
   },
   {
-    location: "Sortal",
-    title: "Intern",
-    period: ["2018 Nov", "2019 Feb"],
-    skills: ["Node.js", "TypeScript", "Docker", "Tensorflow.js", "RabbitMQ", "MongoDB"],
-    description: "Developed multiple modules to integrate into a microservice architecture. Including: A modular TensorFlow service (TFJS), capable of dynamically loading and caching pre-trained models. A module utilizing Google Vision's API. A module to analyze and apply custom classifications based on existing labels.",
+    location: 'Sortal',
+    title: 'Intern',
+    period: ['2018 Nov', '2019 Feb'],
+    skills: [
+      'Node.js',
+      'TypeScript',
+      'Docker',
+      'Tensorflow.js',
+      'RabbitMQ',
+      'MongoDB',
+    ],
+    description:
+      "Developed multiple modules to integrate into a microservice architecture. Including: A modular TensorFlow service (TFJS), capable of dynamically loading and caching pre-trained models. A module utilizing Google Vision's API. A module to analyze and apply custom classifications based on existing labels.",
   },
   {
-    location: "Weko Health",
-    title: "Intern",
-    period: ["2018 Nov", "2019 Jan"],
-    skills: ["Leaflet.js", "Wordpress"],
-    description: "Developed a graphical map representation of Australias PHN network (Leaflet) and a revenue calculator based on projected profits from utilizing WeKo services compatible with Wordpress",
+    location: 'Weko Health',
+    title: 'Intern',
+    period: ['2018 Nov', '2019 Jan'],
+    skills: ['Leaflet.js', 'Wordpress'],
+    description:
+      'Developed a graphical map representation of Australias PHN network (Leaflet) and a revenue calculator based on projected profits from utilizing WeKo services compatible with Wordpress',
   },
   {
-    location: "BlockNet",
-    title: "Developer",
-    period: ["2018 Jul", "2018 Nov"],
-    skills: ["Node.js", "React.js", "Solidity"],
-    description: "A server-less forum web application based on decentralised storage and the Block chain for peer to peer communication. Built on IPFS for redundant storage and Ethereum for security.",
+    location: 'BlockNet',
+    title: 'Developer',
+    period: ['2018 Jul', '2018 Nov'],
+    skills: ['Node.js', 'React.js', 'Solidity'],
+    description:
+      'A server-less forum web application based on decentralised storage and the Block chain for peer to peer communication. Built on IPFS for redundant storage and Ethereum for security.',
   },
   {
-    location: "L-Player",
-    title: "Scrum Master",
-    period: ["2018 Mar", "2018 Jun"],
-    skills: ["Node.js", "React.js"],
-    description: "Coordinated a team of six with the Principal Research Fellow of the UQ Centre for Horticultural Science to design, and prototype a web based implementation of their plant simulation software CPFG. The project was developed under the scrum framework requiring fortnightly deliverables and constant communication.",
+    location: 'L-Player',
+    title: 'Scrum Master',
+    period: ['2018 Mar', '2018 Jun'],
+    skills: ['Node.js', 'React.js'],
+    description:
+      'Coordinated a team of six with the Principal Research Fellow of the UQ Centre for Horticultural Science to design, and prototype a web based implementation of their plant simulation software CPFG. The project was developed under the scrum framework requiring fortnightly deliverables and constant communication.',
   },
   {
-    location: "Boeing Glider",
-    title: "Manufacturing and 3D Modelling",
-    period: ["2017 Jul", "2018 Nov"],
+    location: 'Boeing Glider',
+    title: 'Manufacturing and 3D Modelling',
+    period: ['2017 Jul', '2018 Nov'],
     skills: [],
-    description: "Collaborated with a team to design, simulate and manufacture a low altitude glider in PTC Creo and ANSYS. Additionally, programmed and manufactured a flight data recorder to measure flight metrics with personally modelled and 3D printed enclosure, wings and tail.",
+    description:
+      'Collaborated with a team to design, simulate and manufacture a low altitude glider in PTC Creo and ANSYS. Additionally, programmed and manufactured a flight data recorder to measure flight metrics with personally modelled and 3D printed enclosure, wings and tail.',
   },
   {
-    location: "Backhoe Excavator",
-    title: "Design and Manufacturing",
-    period: ["2016 Mar", "2018 June"],
+    location: 'Backhoe Excavator',
+    title: 'Design and Manufacturing',
+    period: ['2016 Mar', '2018 June'],
     skills: [],
-    description: "Collaborated with a team to design, mathematically proof, and manufacture a remote controlled backhoe excavator with five degrees of motion utilising pneumatic motion.",
+    description:
+      'Collaborated with a team to design, mathematically proof, and manufacture a remote controlled backhoe excavator with five degrees of motion utilising pneumatic motion.',
   },
 ];
 
@@ -128,19 +174,20 @@ export const hobbies = [
   },
   */
   {
-    title: "3D printing\nand modeling",
-    description: "Familiar with CAD and constrained modelling of dimensionally accurate models for hand-built 3D printer with Fusion360",
+    title: '3D printing\nand modeling',
+    description:
+      'Familiar with CAD and constrained modelling of dimensionally accurate models for hand-built 3D printer with Fusion360',
   },
   {
-    title: "Electrical",
-    description: "Circuit analysis, circuit design, and soldering",
+    title: 'Electrical',
+    description: 'Circuit analysis, circuit design, and soldering',
   },
   {
-    title: "Manufacturing",
-    description: "Experience with workshop and power tools",
+    title: 'Manufacturing',
+    description: 'Experience with workshop and power tools',
   },
   {
-    title: "Art",
-    description: "Pen/pencil sketching and drafting",
+    title: 'Art',
+    description: 'Pen/pencil sketching and drafting',
   },
 ];
