a78107296cc3724e2d15cca67c4de827bdabdac8
Hans Song <hans.m.song@gmail.com>
2021-06-14 17:16:03 +1000

    update content

--- src/Info.js
+++ src/Info.js
@@ -1,9 +1,8 @@
 export const profile = {
-  name: 'Hans Song',
-  bio:
-    'Final year studying Software Engineering at the University of Queensland',
-  image: '',
-  resume: '',
+  name: "Hans Song",
+  bio: "Final year studying Software Engineering at the University of Queensland",
+  image: "",
+  resume: "",
 };
 
 export const education = [
@@ -15,14 +14,14 @@ export const education = [
   },
   */
   {
-    location: 'University of Queensland',
-    certification: 'Bachelor of Engineering (Software)',
-    period: ['2016 Feb', 'Present'],
+    location: "University of Queensland",
+    certification: "Bachelor of Engineering (Software)",
+    period: ["2016 Feb", "2021 Jun"],
   },
   {
-    location: 'Queensland Academy for Science, Math, and Technology',
-    certification: 'International Baccalaureate Diploma',
-    period: ['2013 Jan', '2015 Nov'],
+    location: "Queensland Academy for Science, Math, and Technology",
+    certification: "International Baccalaureate Diploma",
+    period: ["2013 Jan", "2015 Nov"],
   },
 ];
 
@@ -33,53 +32,43 @@ export const skills = [
   //     list: [],
   // },
   {
-    title: 'Frontend Development',
+    title: "Frontend Development",
     description:
-      'Experience in standard frontend and backend website development',
-    list: ['HTML/CSS', 'JavaScript/Typescript', 'React.Js', 'Vue.Js'],
+      "Experience in standard frontend and backend website development",
+    list: ["HTML/CSS", "React.Js", "Vue.Js"],
   },
   {
-    title: 'Backend Development',
+    title: "Backend Development",
     description:
-      'Experience in hosting and serving web services and common deployment tools',
+      "Experience in hosting and serving web services and common deployment tools",
+    list: ["Docker", "Docker Compose", "Express.js", "Node.js"],
+  },
+  {
+    title: "Software Development",
+    description:
+      "Experience in OOP, imperative, and functional programming paradigm. Minor experience with embeded development.",
     list: [
-      'Docker',
-      'Docker Compose',
-      'Nginx',
-      'JavaScript/TypeScript',
-      'Express.js',
-      'Node.js',
+      "Git",
+      "Python",
+      "Java",
+      "C",
+      "C++",
+      "JavaScript",
+      "Typescript",
+      "SQL",
+      "MatLab",
     ],
   },
   {
-    title: 'Software Development',
+    title: "3D Modelling and Printing",
     description:
-      'Experience in OOP, imperative, and functional programming paradigm. Minor experience with embeded development.',
-    list: ['Python', 'Java', 'C', 'HTML/CSS', 'SQL', 'MatLab'],
+      "Familiar with CAD and constrained modelling of dimensionally accurate models for printing on hand-built 3D printer.",
+    list: ["AutoCAD", "Inventor", "Fusion360"],
   },
   {
-    title: 'Software Libraries',
-    description: 'Familiar with popular libraries and frameworks.',
-    list: [
-      'JUnit',
-      'Express.js',
-      'Puppeteer',
-      'Playwright',
-      'RabbitMQ',
-      'MongoDB',
-      'Jest/Mocha+Chai',
-    ],
-  },
-  {
-    title: '3D Modelling and Printing',
-    description:
-      'Familiar with CAD and constrained modelling of dimensionally accurate models for printing on hand-built 3D printer.',
-    list: ['AutoCAD', 'Inventor', 'Fusion360'],
-  },
-  {
-    title: 'Languages',
-    description: 'Proficient in Mandarin Chinese.',
-    list: ['HSK 4'],
+    title: "Languages",
+    description: "Proficient in Mandarin Chinese.",
+    list: ["HSK 4"],
   },
 ];
 
@@ -94,75 +83,74 @@ export const experience = [
   },
   */
   {
-    location: 'Credfin Australia',
-    title: 'Software Developer',
-    period: ['2019 Jan', 'Present'],
+    location: "Credfin Australia",
+    title: "Software Developer",
+    period: ["2019 Jan", "Present"],
     skills: [
-      'Node.js',
-      'JavaScript',
-      'TypeScript',
-      'Puppeteer',
-      'Playwright',
-      'Vue.js',
-      'SQL',
+      "Node.js",
+      "JavaScript",
+      "TypeScript",
+      "Puppeteer",
+      "React.js",
+      "Vue.js",
     ],
     description:
-      'Creating and maintaining autonomous data aggregation modules utilizing puppeteer and creating tools for developers.',
+      "Creating and maintaining autonomous data aggregation modules utilizing puppeteer and creating tools to enhance developer performance.",
   },
   {
-    location: 'Sortal',
-    title: 'Intern',
-    period: ['2018 Nov', '2019 Feb'],
+    location: "Sortal",
+    title: "Intern",
+    period: ["2018 Nov", "2019 Feb"],
     skills: [
-      'Node.js',
-      'TypeScript',
-      'Docker',
-      'Tensorflow.js',
-      'RabbitMQ',
-      'MongoDB',
+      "Node.js",
+      "TypeScript",
+      "Docker",
+      "Tensorflow.js",
+      "RabbitMQ",
+      "MongoDB",
     ],
     description:
       "Developed multiple modules to integrate into a microservice architecture. Including: A modular TensorFlow service (TFJS), capable of dynamically loading and caching pre-trained models. A module utilizing Google Vision's API. A module to analyze and apply custom classifications based on existing labels.",
   },
   {
-    location: 'Weko Health',
-    title: 'Intern',
-    period: ['2018 Nov', '2019 Jan'],
-    skills: ['Leaflet.js', 'Wordpress'],
+    location: "Weko Health",
+    title: "Intern",
+    period: ["2018 Nov", "2019 Jan"],
+    skills: ["Leaflet.js", "Wordpress"],
     description:
-      'Developed a graphical map representation of Australias PHN network (Leaflet) and a revenue calculator based on projected profits from utilizing WeKo services compatible with Wordpress',
+      "Developed a graphical map representation of Australias PHN and a revenue calculator based on projected profits from utilizing WeKo services compatible with Wordpress",
   },
   {
-    location: 'BlockNet',
-    title: 'Developer',
-    period: ['2018 Jul', '2018 Nov'],
-    skills: ['Node.js', 'React.js', 'Solidity'],
+    location: "BlockNet",
+    title: "Developer",
+    period: ["2018 Jul", "2018 Nov"],
+    skills: ["Node.js", "React.js", "Solidity"],
     description:
-      'A server-less forum web application based on decentralised storage and the Block chain for peer to peer communication. Built on IPFS for redundant storage and Ethereum for security.',
+      "A server-less forum web application based on decentralised storage and the Block chain for peer to peer communication. Built on IPFS for redundant storage and Ethereum for security.",
   },
   {
-    location: 'L-Player',
-    title: 'Scrum Master',
-    period: ['2018 Mar', '2018 Jun'],
-    skills: ['Node.js', 'React.js'],
+    location: "L-Player",
+    title: "Scrum Master",
+    period: ["2018 Mar", "2018 Jun"],
+    skills: ["Node.js", "React.js"],
     description:
-      'Coordinated a team of six with the Principal Research Fellow of the UQ Centre for Horticultural Science to design, and prototype a web based implementation of their plant simulation software CPFG. The project was developed under the scrum framework requiring fortnightly deliverables and constant communication.',
+      "Coordinated a team of six with the Principal Research Fellow of the UQ Centre for Horticultural Science to design, and prototype a web based implementation of their plant simulation software CPFG. The project was developed under the scrum framework requiring fortnightly deliverables and constant communication.",
   },
   {
-    location: 'Boeing Glider',
-    title: 'Manufacturing and 3D Modelling',
-    period: ['2017 Jul', '2018 Nov'],
+    location: "Boeing Glider",
+    title: "Manufacturing and 3D Modelling",
+    period: ["2017 Jul", "2018 Nov"],
     skills: [],
     description:
-      'Collaborated with a team to design, simulate and manufacture a low altitude glider in PTC Creo and ANSYS. Additionally, programmed and manufactured a flight data recorder to measure flight metrics with personally modelled and 3D printed enclosure, wings and tail.',
+      "Collaborated with a team to design, simulate and manufacture a low altitude glider in PTC Creo and ANSYS. Additionally, programmed and manufactured a flight data recorder to measure flight metrics with personally modelled and 3D printed enclosure, wings and tail.",
   },
   {
-    location: 'Backhoe Excavator',
-    title: 'Design and Manufacturing',
-    period: ['2016 Mar', '2018 June'],
+    location: "Backhoe Excavator",
+    title: "Design and Manufacturing",
+    period: ["2016 Mar", "2018 June"],
     skills: [],
     description:
-      'Collaborated with a team to design, mathematically proof, and manufacture a remote controlled backhoe excavator with five degrees of motion utilising pneumatic motion.',
+      "Collaborated with a team to design, mathematically proof, and manufacture a remote controlled backhoe excavator with five degrees of motion utilising pneumatic motion.",
   },
 ];
 
@@ -174,20 +162,20 @@ export const hobbies = [
   },
   */
   {
-    title: '3D printing\nand modeling',
+    title: "3D printing\nand modeling",
     description:
-      'Familiar with CAD and constrained modelling of dimensionally accurate models for hand-built 3D printer with Fusion360',
+      "Familiar with CAD and constrained modelling of dimensionally accurate models for hand-built 3D printer with Fusion360",
   },
   {
-    title: 'Electrical',
-    description: 'Circuit analysis, circuit design, and soldering',
+    title: "Electrical",
+    description: "Circuit analysis, circuit design, and soldering",
   },
   {
-    title: 'Manufacturing',
-    description: 'Experience with workshop and power tools',
+    title: "Manufacturing",
+    description: "Experience with workshop and power tools",
   },
   {
-    title: 'Art',
-    description: 'Pen/pencil sketching and drafting',
+    title: "Art",
+    description: "Pen/pencil sketching and drafting",
   },
 ];
